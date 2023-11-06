8d7d0c2f401c9f58aeafd0a9d4957ea475e9cd2c
Morteza Rostami <morteza.rostami.55b@gmail.com>
2023-11-06 13:52:55 +0400

    added express server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,2 +1,22 @@
+import express from 'express'
+import morgan from 'morgan';
 
-console.log('love');
+// anything from .env is a string
+const PORT = Number(process.env.PORT) || 8080;
+
+const app = express();
+
+// set Logging lib
+app.use(morgan('dev'));
+
+// catch all route
+app.use('*', (req, res) => {
+  res.json({
+    msg: '404!',
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`node-server running on port ${PORT}`);
+});
+
